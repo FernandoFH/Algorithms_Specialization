de4bced96d3cfaed4af6193ae7533475ad67d7ac
FernandoH <ffhf.1820@gmail.com>
2026-01-24 01:59:07 -0300

    UPDATE: Leet Code Problem 1 , 2 and 3

---
+++ leetCode/cpp/problem_0003/longest_substring_without_repeating_characters.test.cpp
@@ -0,0 +1,13 @@
+#define CATCH_CONFIG_MAIN
+#include "../catch_amalgamated.hpp"
+#include "longest_substring_without_repeating_characters.cpp"
+
+TEST_CASE("lengthOfLongestSubstring", "[longest_substring_without_repeating_characters]") {
+    // TODO: Add test cases
+    REQUIRE(1 == 1);
+    
+    // Example test case (uncomment and modify):
+    // Solution solution;
+    // auto result = solution.lengthOfLongestSubstring(...);
+    // REQUIRE(result == expected);
+}
